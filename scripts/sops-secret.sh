b921396232adfeab86cf725da7ce782d43ca5b24
brandtkeller <info@brandtkeller.net>
2021-07-11 20:10:05 -0700

    feat: adding cluster issuer and CA secret information

--- scripts/sops-secret.sh
+++ scripts/sops-secret.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Helper script to add gpg secret to cluster. Need to auto-parse the gpg key id instead of needing to copy/paste.
+
 gpg --export-secret-keys \
   --armor $1 |
 kubectl create secret generic sops-gpg \
   --namespace=$2 \
-  --from-file=sops.asc=/dev/stdin--help
+  --from-file=sops.asc=/dev/stdin
